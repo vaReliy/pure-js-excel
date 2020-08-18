a876786fdc3a99089ec0fc2de72d3418cc0f9a81
Valeriy Garbuzov <valeriy@i.ua>
2020-08-18 11:24:04 +0300

    moved resize logic to apart file

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,5 @@
+import {resizeHandler, shouldResize} from '@/components/table/table.functions';
 import {getTemplateTable} from '@/components/table/table.template';
-import {$} from '@core/Dom';
 import {ExcelComponent} from '@core/ExcelComponent';
 
 export class Table extends ExcelComponent {
@@ -17,49 +17,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    const $resizer = $(event.target);
-    const resizeTarget =$resizer.data.resize;
-    if (resizeTarget) {
-      const $parent = $resizer.closest('[data-resizable]');
-      const isColumn = resizeTarget === 'column';
-      const rect = $parent.boundingClientRect;
-      const parentId = $parent.text.toLowerCase();
-      const targetCellAttribute = `[data-resizable-${parentId}]`;
-      const resizerCssProp = isColumn ? 'right' : 'bottom';
-
-      let delta;
-      let widthValue;
-      document.onmousemove = event => {
-        if (isColumn) {
-          delta = event.clientX - rect.right;
-          widthValue = `${rect.width + delta}px`;
-        } else {
-          delta = event.clientY - rect.bottom;
-        }
-        $resizer.css({
-          [resizerCssProp]: `${-delta}px`,
-        });
-      };
-
-      document.onmouseup = () => {
-        if (isColumn) {
-          $parent.css({
-            width: widthValue,
-          });
-          document.querySelectorAll(targetCellAttribute).forEach(cell => {
-            cell.style.width = widthValue;
-          });
-        } else {
-          $parent.css({
-            height: `${rect.height + delta}px`,
-          });
-        }
-        $resizer.css({
-          [resizerCssProp]: 0,
-        });
-        document.onmousemove = null;
-        document.onmouseup = null;
-      };
+    if (shouldResize(event)) {
+      resizeHandler(event);
     }
   }
 }
---
+++ src/components/table/table.functions.js
@@ -0,0 +1,49 @@
+import {$} from '@core/Dom';
+
+export function shouldResize(event) {
+  return event.target.dataset.resize;
+}
+
+export function resizeHandler(event) {
+  const $resizer = $(event.target);
+  const $parent = $resizer.closest('[data-resizable]');
+  const isColumn = $resizer.data.resize === 'column';
+  const rect = $parent.boundingClientRect;
+  const parentId = $parent.text.toLowerCase();
+  const targetCellAttribute = `[data-resizable-${parentId}]`;
+  const resizerCssProp = isColumn ? 'right' : 'bottom';
+
+  let delta;
+  let widthValue;
+  document.onmousemove = event => {
+    if (isColumn) {
+      delta = event.clientX - rect.right;
+      widthValue = `${rect.width + delta}px`;
+    } else {
+      delta = event.clientY - rect.bottom;
+    }
+    $resizer.css({
+      [resizerCssProp]: `${-delta}px`,
+    });
+  };
+
+  document.onmouseup = () => {
+    if (isColumn) {
+      $parent.css({
+        width: widthValue,
+      });
+      document.querySelectorAll(targetCellAttribute).forEach(cell => {
+        cell.style.width = widthValue;
+      });
+    } else {
+      $parent.css({
+        height: `${rect.height + delta}px`,
+      });
+    }
+    $resizer.css({
+      [resizerCssProp]: 0,
+    });
+    document.onmousemove = null;
+    document.onmouseup = null;
+  };
+}
