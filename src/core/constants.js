bb84a2844171b6e35fc99e6ecb77eff66946e0bb
Valeriy Garbuzov <valeriy@hydraglobe.com>
2020-09-09 14:16:04 +0300

    Updates

--- src/core/constants.js
+++
@@ -1,6 +0,0 @@
-export const defaultStyles = {
-  textAlign: 'left',
-  fontWeight: 'normal',
-  fontStyle: 'normal',
-  textDecoration: 'none',
-};
--- src/pages/DashboardPage.js
+++
@@ -1,9 +0,0 @@
-import {dashboardToHTML} from '@/pages/dashboard.template';
-import {Page} from '@/pages/Page';
-import {$} from '@core/Dom';
-
-export class DashboardPage extends Page {
-  getRoot() {
-    return $.create('div', 'db').html(dashboardToHTML());
-  }
-}
